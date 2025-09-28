f6d440efe326e11d7957860a6d2e0f96a8d06f9c
Alamin Hossain <ytakashahmed@gmail.com>
2025-09-28 21:52:23 +0600

    Reload issue

--- src/pages/AddCourse.jsx
+++ src/pages/AddCourse.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useAuthContext from '../hooks/useAuthContext';
 import CreateCourse from '../Create/CreateCourse';
 
 const AddCourse = () => {
     const {user}=useAuthContext()
+    // const [loading,setLoading]=useState(false)
     return (
         <div>
               {(user.role==='admin' || user.role=='teacher') &&(
